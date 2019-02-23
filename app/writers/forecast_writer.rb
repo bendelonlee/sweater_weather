0afd42499323c02eb53d5f4f02ce10d6356ee727
Ben Lee <bendelonlee@gmail.com>
2019-02-23 16:12:02 -0700

    Amends logic in writer so forecasts are checked before fetching

--- app/writers/forecast_writer.rb
+++ app/writers/forecast_writer.rb
@@ -1,14 +1,20 @@
 class ForecastWriter
   def find_or_fetch(args)
     @city = args[:city]
-    require 'pry'; binding.pry
-    return forecast = @city.forecast if forecast
-    Forecast.from_service( service, @city )
+    if forecast
+      forecast
+    else
+      Forecast.from_service( service, @city )
+    end
   end
 
   private
 
   def service
     DarkSkyService.new(@city)
+  end
+
+  def forecast
+    @city.forecast
   end
 end
