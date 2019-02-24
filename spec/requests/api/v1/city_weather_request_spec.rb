d2e3ec24b2aae233d75c1363584b31027d03771a
Ben Lee <bendelonlee@gmail.com>
2019-02-24 07:43:29 -0700

    Adds long list of expects for request

--- spec/requests/api/v1/city_weather_request_spec.rb
+++ spec/requests/api/v1/city_weather_request_spec.rb
@@ -9,10 +9,54 @@ describe 'customer relationship requests' do
     end
 
     expect(response).to be_successful
-    returned_temperature = JSON.parse(response.body)["data"]["temperatures_today"]
+    result = JSON.parse(response.body, symbolize_names: true)
 
-    expect(returned_weather).to have_key("high")
-    expect(returned_weather).to have_key("low")
-    expect(returned_weather).to have_key("current")
+    expect(result).to be_a(Hash)
+
+    expect(result).to have_key(:id)
+    expect(result).to have_key(:day_summary)
+    expect(result).to have_key(:week_summary)
+    expect(result).to have_key(:current_hour)
+    expect(result).to have_key(:current_day)
+    expect(result).to have_key(:hours)
+    expect(result).to have_key(:days)
+
+    current_hour = result[:current_hour]
+    current_day = result[:current_day]
+    hours = result[:hours]
+    days = result[:days]
+
+    expect(current_hour).to have_key :icon
+    expect(current_hour).to have_key :summary
+    expect(current_hour).to have_key :time
+    expect(current_hour).to have_key :temperature
+    expect(current_hour).to have_key :feels_like
+    expect(current_hour).to have_key :humidity
+    expect(current_hour).to have_key :visibility
+    expect(current_hour).to have_key :uv_index
+
+    expect(current_day).to have_key :icon
+    expect(current_day).to have_key :summary
+    expect(current_day).to have_key :time
+    expect(current_day).to have_key :high
+    expect(current_day).to have_key :low
+    expect(current_day).to have_key :precip_probability
+    expect(current_day).to have_key :precip_type
+
+    expect(hours).to be_a(Array)
+    expect(hours.count).to be_between(8, 25)
+    expect(hours.first).to have_key :temperature
+    expect(hours.first).to have_key :time_of_day
+    expect(hours.first.keys.count).to eq(2)
+
+    expect(days).to be_a(Array)
+    expect(days.count).to be_between(7, 8)
+    expect(days.first).to have_key :icon
+    expect(days.first).to have_key :day_of_week
+    expect(days.first).to have_key :high
+    expect(days.first).to have_key :low
+    expect(days.first).to have_key :precip_probability
+    expect(days.first).to have_key :precip_type
+    expect(days.first.keys.count).to eq(6)
   end
 end
