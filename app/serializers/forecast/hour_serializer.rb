3f7c58388b3ea049fdb3fe1dba9d8f60751c9a75
Ben Lee <bendelonlee@gmail.com>
2019-02-26 09:20:54 -0700

    simplifies time of day so it just gives the utc time

--- app/serializers/forecast/hour_serializer.rb
+++ app/serializers/forecast/hour_serializer.rb
@@ -1,6 +1,6 @@
 class Forecast::HourSerializer < ActiveModel::Serializer
   attributes :temperature, :time_of_day
   def time_of_day
-    object.time.strftime('%l %p')
+    object.time.utc
   end
 end
